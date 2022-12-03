80c1a85cdb5156b794ffd199cd198bb22a5543d9
Arbaz Nandgaon <arbaznandgaon@gmail.com>
2022-12-03 22:48:27 +0530

    props

--- src/ComponentFile/ChatScreen.jsx
+++ src/ComponentFile/ChatScreen.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+import AccessibilityIcon from '@mui/icons-material/Accessibility';
 export class ChatScreen extends Component {
   render() {
-    return (
+    return ( 
       <div className='bg-blue-600 relative col-span-3 h-screen'>
         <div className='bg-gray-300 text-3xl h-12 sticky top-0 '>ChatScreen</div>
-        <div className='bg-emerald-600 text-5xl  pl-[20rem] pt-[17rem] h-screen'>Msg Display</div>
+        <div className='bg-emerald-600 text-5xl   h-screen'><AccessibilityIcon/></div>
         <div className='bg-slate-700 w-full h-12 text-3xl absolute bottom-0'>ChatScreen</div>
 
       </div>
