c0cc509d98a50a4a4e5a2efc4fea528647aca7e5
Arbaz Nandgaon <arbaznandgaon@gmail.com>
2022-12-17 19:05:57 +0530

    Promises

--- src/App.js
+++ src/App.js
@@ -1,30 +1,80 @@
-import "./App.css";
 import HomeScreen from "./Screens/HomeScreen";
 import { useState } from "react";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import Change1 from "./work/Change1";
 import Login from "./ComponentFile/Login";
-import Change from "./work/Change";
 import app from './firebase'
 
 
 
 function App() {
-  const [user, setUser] = useState(false);
-  const [mor,setMor] = useState(null)
+  const [user, setUser] = useState(null);
+  // // const [mor,setMor] = useState(null)
 
   const auth= getAuth(app);
   const provider = new GoogleAuthProvider();
 
+
+  async function Signup(){
+  await  signInWithPopup(auth, provider)
+    .then((result) => {     
+      const data = result.user;
+      setUser(data);
+      // ...
+    }).catch((error) => {
+      console.log(error)
+      setUser(null)
+    });
   
+
+  }
+
+
+
+// async function A(){
+//   console.log("A")
+ 
+// }
+//  function B(){
+//   console.log("B")
+// }
+// A();
+// B();
+// d();
+// c();
+
+// let value= new Promise(function (A,B){
+//   let name= "Arbaz111";
+//   if  (name==="Arbaz"){
+//     A("Got It")
+//   }
+//   else {
+//     B("Didnt get It")
+//   }
+// })
+
+// value.then(function(va){
+//   console.log(va)
+// }).catch(function(av){
+// console.log(av)
+// })
+
+async function ABC(){
+  let ppo = new Promise(function (A){
+    A(500)
+  })
+  let newval =  await ppo.then(function(r){
+    
+  })
+}
+
+console.log(ABC())
+
 
   return (
     <div> 
     <div className=" bg-gray-900 h-screen w-screen">
-      {user ? <HomeScreen /> : <Login  login={setUser} />} 
-
-
-
+      {user ? <HomeScreen /> : <Login  login={Signup} />} 
+      
 
     {/* {mor ? <Change1/>: <Change getin={setMor}/>} */}
  
--- src/ComponentFile/Login.jsx
+++ src/ComponentFile/Login.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function Login(props) {
-  console.log(props)
+
   return (
     <div >
-      <div className="pt-20 bg-gradient-to-r from-indigo-500 to-red-300 h-screen">
-    <div className='h-72 px-2 space-y-8 pt-12  ml-[35rem]   w-1/5 bg-purple-300'>
+      <div className="pt-20  bg-gradient-to-r from-indigo-500 to-red-300 h-screen">
+    <div className='h-72 px-2 space-y-8 pt-12 rounded-lg  ml-[35rem]   w-1/5 bg-purple-300'>
        <div className='ml-12 '>
        <div >
        <label>UserName</label><br></br>
@@ -18,6 +18,13 @@ function Login(props) {
        <div>
         <button onClick={props.login}  className='bg-gray-200 w-max px-2 py-1 mt-6 font-bold ml-16'>Login</button>
        </div>
+       <a className='ml-20 mt-4'>OR</a>
+       <div className="mt-2 ml-12 flex space-x-3">
+                <button onClick={()=> props.login()} className=" flex items-center space-x-1 px-4 border py-[6px] w-1/2 justify-center rounded-md">
+                    <img className=" h-5 w-5" src="//heathmont.imgix.net/bitcasino/images/google-oauth2.svg?auto=compress,format" loading="lazy" />
+                        <span>Google</span>
+                </button>
+            </div>
 
        </div>
        </div>
